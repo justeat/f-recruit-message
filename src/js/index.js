37df3121951699a0cdda326da84b45029fa833f2
Simon Sanders <simon.sanders@just-eat.com>
2018-07-20 13:18:31 +0100

    v0.0.2 - Minor text update

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@
  * Write the recruitment message to the console
  */
 const recruitMessage = function () {
-    console.log("\n\n%c %c \n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\nLove web development? Love takeaway? Why not join us?\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n%chttps://careers.just-eat.com/opportunities\n\n",
-    "font-size:50px; margin-left:35px; padding-right:295px; background: url(http://dy3erx8o0a6nh.cloudfront.net/images/logo-just-eat-2016_x2.png) no-repeat;')",
+    console.log("\n\n%c %c \n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\nLove development? Love takeaway? Then why not join us?\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n%chttps://careers.just-eat.com/opportunities\n\n",
+    "font-size:50px; margin-left:35px; padding-right:295px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/logo-just-eat-2016_x2.png) no-repeat;')",
     "font-family: Hind Vadodara, Arial, sans-serif; font-size: 14px; line-height: 1.5; color: #535353; background-color: #fff;",
     "font-family: Hind Vadodara, Arial, sans-serif; font-size: 14px; line-height: 1.5; color: #535353; background-color: #fff; text-decoration: underline; margin-left: 55px");
 };
 
 /**
- * Export f-recruit-message function
+ * Export recruitMessage()
  */
 module.exports = {
 	recruitMessage: recruitMessage
