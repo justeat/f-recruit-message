a38cf81d227431988817338e2edbbedd2ac8b87a
Simon Sanders <simon.sanders@just-eat.com>
2018-10-23 11:59:09 +0100

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * Write the recruitment message to the console
  */
 const recruitMessage = () => {
-    const fontStyle = 'font-family: Hind Vadodara, Arial, sans-serif; font-size: 14px; line-height: 1.5; color: #535353; background-color: #fff;';
-    const fontStyleUnderline = `${fontStyle} text-decoration: underline; margin-left: 120px`;
-    const logo = 'font-size:50px; margin-left:100px; padding-right:230px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/logo-just-eat-2016_x2.png) no-repeat;)';
+    const fontStyle = 'font-family: Hind Vadodara, Arial, sans-serif; font-size: 14px; line-height: 1.5; color: #535353; background-color: #fff; margin-left: 55px';
+    const fontStyleUnderline = `${fontStyle} text-decoration: underline; margin-left: 55px`;
+    const logo = 'font-size:50px; margin-left:100px; padding-right:295px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/logo-just-eat-2016_x2.png) no-repeat;)';
     const rays = 'font-size:150px; margin-left:-55px; margin-top:-90px; padding-right:285px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/color-rays.png) 50% no-repeat;)';
 
     console.log(`
