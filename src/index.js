7d3292702e9f981575b4870d47f93068939857d0
Simon Sanders <simon.sanders@just-eat.com>
2021-04-09 16:23:14 +0100

    f-recruit-message@v0.3.0 - Use orange branding.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@
  * Write the recruitment message to the console
  */
 const recruitMessage = () => {
-    const fontStyle = 'font-family: Hind Vadodara, Arial, sans-serif; font-size: 14px; line-height: 1.5; color: #535353; background-color: #fff; margin-left: 55px';
-    const fontStyleUnderline = `${fontStyle} text-decoration: underline; margin-left: 55px`;
-    const logo = 'font-size:50px; margin-left:100px; padding-right:295px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/logo-just-eat-2016_x2.png) no-repeat;)';
-    const rays = 'font-size:150px; margin-left:-55px; margin-top:-90px; padding-right:285px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/color-rays.png) 50% no-repeat;)';
+    const fontStyle = 'font-family: "JustEatBasis", Arial, sans-serif; font-size: 14px; line-height: 1.5; color: #535353; background-color: #fff; margin-left: 55px';
+    const fontStyleUnderline = `${fontStyle} text-decoration: underline; margin-left: 90px`;
+    const logo = 'font-size:50px; margin-left:100px; padding-right:295px; background: url(https://dy3erx8o0a6nh.cloudfront.net/images/je-165-x-32-red@2x.png) no-repeat;)';
 
     console.log(`
 
@@ -15,10 +14,8 @@ const recruitMessage = () => {
     Love development? Love takeaway? Then why not join us?
     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    %chttps://careers.just-eat.com/opportunities
-
-    %c
-    `, logo, fontStyle, fontStyleUnderline, rays);
+    %chttps://careers.just-eat.com
+    `, logo, fontStyle, fontStyleUnderline);
 };
 
 export default recruitMessage;
